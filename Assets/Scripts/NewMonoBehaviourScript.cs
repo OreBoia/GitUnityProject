80d6d748102fc4a3ea0f8d0e0b772920deb5aa80
Edoardo Del sarto <e99delsarto@gmail.com>
2026-05-08 12:18:01 +0200

    Update MonoBehaviourScript

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, World! branch_edo");
+        Debug.Log("Hello, World!");
     }
 
     // Update is called once per frame
